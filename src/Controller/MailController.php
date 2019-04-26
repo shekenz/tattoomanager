2439916bfbe440bf2dc386e6641ce3ee4ddcc248
shekenz <aureltrotebas@icloud.com>
2019-04-26 14:13:03 +0200

    Added other files missing from previous commit (Again gstat)

---
+++ src/Controller/MailController.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+use Cake\I18n\Time;
+use Session;
+
+/**
+ * Clients Controller
+ *
+ * @property \App\Model\Table\ClientsTable $Clients
+ *
+ * @method \App\Model\Entity\Client[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class ClientsController extends AppController
+{
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|void
+     */
+    public function index()
+    {
+        
+    }
+
+}
